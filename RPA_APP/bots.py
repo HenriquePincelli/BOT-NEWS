ec0debd7fa222983718c45bb0373133eda09fc8c
Henrique Pincelli <pincellihenrique9@gmail.com>
2024-09-14 17:33:16 -0300

    first commit

---
+++ RPA_APP/bots.py
@@ -0,0 +1,24 @@
+from RPA_APP.rpa.procedures import RPAAljazeera
+from RPA_APP.config import EMAIL_ALJAZEERA, SERCH_PHRASE_ALJAZEERA, SHOW_MORE_ALJAZEERA
+
+
+def rpa_aljazeera():
+    """Function to init RPA's"""
+
+    # >>>>>>>>>Aljazeera's RPA>>>>>>>>>
+    # >>>>>>>>>Parameters from robocorp storage>>>>>>>>>
+    email = EMAIL_ALJAZEERA
+    search_phrase = SERCH_PHRASE_ALJAZEERA
+    show_more = SHOW_MORE_ALJAZEERA
+    # <<<<<<<<<Parameters from robocorp storage<<<<<<<<<
+
+    # >>>>>>>>>RPA's Procedure>>>>>>>>>
+    print("=-=" * 24)
+    print("Initiating Aljazeera's RPA")
+    result_aljazeera = RPAAljazeera.aljazeera_news(email=email, search_phrase=search_phrase, show_more=show_more)
+    print(">" * 45)
+    print(result_aljazeera)
+    print("<" * 45)
+    print("=-=" * 24)
+    # <<<<<<<<<RPA's Procedure<<<<<<<<<
+    # <<<<<<<<<Aljazeera's RPA<<<<<<<<<
---
+++ tasks.py
@@ -0,0 +1,28 @@
+from RPA_APP.bots import rpa_aljazeera
+from robocorp.tasks import task
+import sys
+import json
+
+
+@task
+def init_rpa():
+    try:
+        rpa_aljazeera()
+    except Exception as e:
+            # >>>>>>>>>Tracing the Error>>>>>>>>>
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            traceback_details = {
+                'filename': exc_traceback.tb_frame.f_code.co_filename,
+                'line_number': exc_traceback.tb_lineno,
+                'function_name': exc_traceback.tb_frame.f_code.co_name,
+                'exception_type': exc_type.__name__,
+                'exception_message': str(exc_value)
+            }
+            print("=-==-==-=ERROR=-==-==-=")
+            print(traceback_details)
+            print("=-==-==-=ERROR=-==-==-=")
+            # <<<<<<<<<Tracing the Error<<<<<<<<<
+            print({"status": False, "msg": json.dumps(traceback_details)})
+    finally:
+        # A place for teardown and cleanups. (Playwright handles browser closing)
+        print("Automation finished!")
